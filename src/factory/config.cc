02d9387ee24984764e5280f352c9488cab672eaf
Newton Albert <126969936+JasonAlbertNewton@users.noreply.github.com>
2023-06-28 23:25:16 +0800

    last update

--- src/factory/config.cc
+++ src/factory/config.cc
@@ -1,3 +1,25 @@
 /*
  * a config file for creating socket class easily
  * */
+
+#ifdef CONFIG
+#else
+#define CONFIG
+
+class Global_Config{
+    public:
+        int socket_descriptor;
+        const int length_of_address;
+
+    private:
+
+
+};
+
+
+#endif
+
+//testing purpose only
+int main(){
+    return 0;
+}
